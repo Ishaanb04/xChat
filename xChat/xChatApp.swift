9060ec0091272bc9c78e6551dbfe3916895a38b2
Ishaan Bhasin <ibhasin@terpmail.umd.edu>
2024-05-23 00:33:31 -0700

    Initial Commit

---
+++ xChat/xChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  xChatApp.swift
+//  xChat
+//
+//  Created by Ishaan Bhasin on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct xChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
